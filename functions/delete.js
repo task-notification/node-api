75f210635b0bf3e412cee9777d57b139e0ebe22b
Michael Müller <mishbag@reu-network.de>
2016-11-27 13:17:27 +0100

    added functions

--- functions/delete.js
+++ functions/delete.js
@@ -5,3 +5,19 @@
  *
  * @author Michael Müller <development@reu-network.de>
  */
+
+var mongoose = require('mongoose');
+var request = require('request');
+var device = require('../models/device');
+var constants = require('../constants/constants.json');
+
+exports.removeDevice = function (registrationId, callback) {
+
+    device.findOneAndRemove({registrationId: registrationId}, function (err) {
+        if (!err)
+            callback(constants.success.msg_del_success);
+        else
+            callback(constants.error.msg_del_failure);
+    });
+
+}
--- functions/devices.js
+++ functions/devices.js
@@ -5,3 +5,16 @@
  *
  * @author Michael Müller <development@reu-network.de>
  */
+
+var mongoose = require('mongoose');
+var request = require('request');
+var device = require('../models/device');
+var constants = require('../constants/constants.json');
+
+exports.listDevices = function (callback) {
+    device.findAll(function (err, devices) {
+        if (!err)
+            callback(devices);
+    });
+}
+
